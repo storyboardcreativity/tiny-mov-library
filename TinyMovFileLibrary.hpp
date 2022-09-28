bf746daaf69fc9a03cf2d5bd49c9ae34965448ca
xhrust <storyboardcreativity@gmail.com>
2022-09-28 08:05:14 -0400

    Support bswap for different platforms

--- TinyMovFileLibrary.hpp
+++ TinyMovFileLibrary.hpp
@@ -8,7 +8,26 @@
 #include <ostream>
 #include <sstream>
 #include <memory.h>
+
+#ifdef _MSC_VER
+
+#include <stdlib.h>
+#define bswap_16(x) _byteswap_ushort(x)
+#define bswap_32(x) _byteswap_ulong(x)
+#define bswap_64(x) _byteswap_uint64(x)
+
+#elif defined(__APPLE__)
+
+#include <libkern/OSByteOrder.h>
+#define bswap_16(x) OSSwapInt16(x)
+#define bswap_32(x) OSSwapInt32(x)
+#define bswap_64(x) OSSwapInt64(x)
+
+#else
+
 #include <byteswap.h>
+
+#endif
 
 #ifndef __TINY_MOV_FILE_BASE_IMPL__
 #define __TINY_MOV_FILE_BASE_IMPL__
